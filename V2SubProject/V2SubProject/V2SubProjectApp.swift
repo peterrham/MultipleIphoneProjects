8441bbb122f0ccbea5d0c7fbc6721b10ca368b6e
Peter Ham <peterrham@gmail.com>
2024-11-09 08:35:29 -0800

    testing creating mimimal first project in multiple project git repository

---
+++ V2SubProject/V2SubProject/V2SubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  V2SubProjectApp.swift
+//  V2SubProject
+//
+//  Created by Ham, Peter on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct V2SubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
