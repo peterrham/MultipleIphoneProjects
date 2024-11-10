f8844aa10a92e49f3bbf4ea0af2d2397d0f04ca5
Peter Ham <peterrham@gmail.com>
2024-11-10 13:59:53 -0800

    First version in sub directory of repository

---
+++ V1NoScopes/V1NoScopesApp.swift
@@ -0,0 +1,16 @@
+//
+//  V1NoScopesApp.swift
+//  V1NoScopesApp
+//
+//  Created by Ham, Peter on 11/10/24.
+//
+import SwiftUI
+
+@main
+struct V1NoScopesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, World!")
+        }
+    }
+}
