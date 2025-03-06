e390101a8013f4323320c51b1bcb0f5e95b21b62
Peter Ham <peterrham@gmail.com>
2025-03-06 06:56:52 -0800

    it all works on the old mac book pro

---
+++ V1ObjRecog/Photo Directly to Google Drive/Photo_Directly_to_Google_DriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Directly_to_Google_DriveApp.swift
+//  Photo Directly to Google Drive
+//
+//  Created by Ham, Peter on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Directly_to_Google_DriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
